ac5a3d56c5a64836a4d55242816f58cf099b7299
Robert Taylor <robert.taylor426@gmail.com>
2013-08-11 19:20:42 +0100

    Updating BootStrap to create valid venues on dev

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -9,7 +9,12 @@ class BootStrap {
 			development {
 				(MONDAY..SUNDAY).each { day ->
 					(1d..10d).each {
-						GormVenue venue = new GormVenue(name: "place-" + it, latitude: 1.0 + it/10, longitude: 0.1 + it/10)
+						GormVenue venue = new GormVenue(name: "place-" + it, 
+							latitude: 1.0 + it/10, 
+							longitude: 0.1 + it/10,
+							addressLine1: '123 Some Street',
+							city: 'London',
+							postcode: 'postcode')
 						venue.addToOpenPeriods(day: day, openHour: 8, closeHour: 30)
 						venue.save()
 					}
