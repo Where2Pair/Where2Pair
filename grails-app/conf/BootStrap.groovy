c480d0e4321c92885e55d12e8cff284e5e42e30d
Robert Taylor <robert.taylor426@gmail.com>
2013-08-13 20:34:44 +0100

    Simulating venue features in Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,14 +7,19 @@ class BootStrap {
     def init = { servletContext ->
 		environments {
 			development {
+				
+				Set availableFeatures = ['wifi', 'mobile payments', 'baby changing']
+				Random random = new Random()
 				(MONDAY..SUNDAY).each { day ->
 					(1d..10d).each {
+						Set features = availableFeatures.findResults { if (random.nextBoolean()) return it }
 						GormVenue venue = new GormVenue(name: "place-" + it, 
 							latitude: 1.0 + it/10, 
 							longitude: 0.1 + it/10,
 							addressLine1: '123 Some Street',
 							city: 'London',
-							postcode: 'postcode')
+							postcode: 'postcode',
+							features: features)
 						venue.addToOpenPeriods(day: day, openHour: 8, closeHour: 30)
 						venue.save()
 					}
