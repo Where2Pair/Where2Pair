e1f2eb3ceae0bf2a10364684cb8e3c857c43c1b3
Mashooq Badar <mashooq.badar@gmail.com>
2013-08-23 00:11:11 +0100

    added initial list of venues

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,21 +1,55 @@
 import groovy.json.JsonBuilder
 
+import static org.where2pair.DayOfWeek.MONDAY
+import static org.where2pair.DayOfWeek.SUNDAY
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 
 def indexPages = ["index.html"] as String[]
+def venues = []
+
+Set availableFeatures = ['wifi', 'mobile payments', 'baby changing']
+Random random = new Random()
+(MONDAY..SUNDAY).each { day ->
+    (1d..10d).each {
+        Set features = availableFeatures.findResults { if (random.nextBoolean()) return it }
+        def venue = [
+                id: random.nextLong().abs().toString(),
+                name: "place-" + it,
+                latitude: 1.0 + it/10,
+                longitude: 0.1 + it/10,
+                addressLine1: '123 Some Street',
+                city: 'London',
+                postcode: 'postcode',
+                features: features,
+                openPeriods: [day: day, openHour: 8, closeHour: 30]
+        ]
+        venues << venue
+    }
+}
 
 ratpack {
     handlers {
-        def venues = [[id: "2123123213", name: "a venue"], [id: '990-9090', name: 'another venue']]
-        get("venues") {
-            def json = new JsonBuilder(venues)
-            response.send json.toString()
+        prefix("venues") {
+            get {
+                def json = new JsonBuilder(venues)
+                response.send json.toString()
+            }
+            get("venue/:venueId") {
+                def venue = venues.findAll { it.id == pathTokens.venueId }
+                def json = new JsonBuilder(venue)
+                response.send json.toString()
+            }
+            get("findNearest") {
+                response.send "nearest venues"
+            }
+            post("venue") {
+                def json = new JsonBuilder(params)
+                venue << json
+                response.send json
+            }
         }
-        get("venue:venueId") {
-            def venue = venues.findAll { it.id == pathTokens.venueId }
-            def json = new JsonBuilder(venue)
-            response.send json.toString()
-        }
+
+
 
         assets "public", indexPages
     }
